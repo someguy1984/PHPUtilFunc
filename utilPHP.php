b924a013482ce8d171cb40fc9d1b94a78b19469e
Not So Dead Account <Not So Dead Account>
2016-03-12 20:37:01 +0000

    Render Function

--- utilPHP.php
+++ utilPHP.php
@@ -2,7 +2,17 @@
 //	PHP Singleton
 */
 
-class utilPHP.php {
+class utilPHP {
 	
+	function utilPHP() {
+		
+	}
 	
+	//Render Function (String, Array (Key {Match} Replace Content)
+	function render($template, $data = array()) {
+		if (!is_array($data)) return null;
+		foreach($data as $key => $val)
+			$template = str_replace("{{$key}}", $val, $template);
+		return $template;
+	}
 }
