a206507a6283613b8b95be61cdc3d7b70df98b9b
adsy2010 <adsy2010@users.noreply.github.com>
2016-08-04 08:07:41 +0100

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 21/07/16
+ * Time: 11:45
+ */
+
+class Database {
+    /** @var MySQLi $db */
+    private $db;
+
+    /**
+     * Can only be called by another database class.
+     * Uses config.ini to determine username, password and database
+     * Connects to a MySQL database using MySQLi
+     */
+    protected function connect()
+    {
+        $config = parse_ini_file("db/config.ini");
+
+        $this->db = new MySQLi();
+        $this->db->connect($config['host'], $config['username'], $config['password'], $config['database']);
+    }
+
+    /**
+     * Returns the database object to database classes
+     * @return MySQLi A MySQLi object
+     */
+    protected function getDatabase(){ return $this->db; }
+
+    /**
+     * Returns any error the database has thrown
+     * @return string A MySQLi database error description
+     */
+    public function getDatabaseError() { return $this->db->error; }
+
+    /**
+     * Returns any error number the database has thrown
+     * @return string A MySQLi database error number
+     */
+    public function getDatabaseErrorNo(){ return $this->db->errno; }
+}
