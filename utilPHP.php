381eefe0f1daa25b33f87ff6bd3ecf64e69205ae
adsy2010 <adsy2010@users.noreply.github.com>
2017-08-17 00:03:19 +0100

    Added mergestrings, outputlink and output table

--- utilPHP.php
+++ utilPHP.php
@@ -15,4 +15,54 @@ class utilPHP {
 			$template = str_replace("{{$key}}", $val, $template);
 		return $template;
 	}
+
+	function mergeStrings(...$string)
+        {
+            $str = "";
+            foreach ($string as $s)
+                $str .= $s;
+
+            return $str;
+        }
+	function outputLink($url, $text = null)
+    	{
+            return Utility::render("<a href='{$url}'>{text}</a>", 
+                array("text" => ($text ==  null) ? $url:$text));
+	}
+
+	public static function outputTable($headers = array(), $data = array())
+    	{
+            $tableTpl = "<table><thead><tr>{head}</tr></thead><tbody>{data}</tbody></table>";
+            $columnCount = count($headers);
+
+            $header = array();
+
+            foreach ($headers as $heading)
+            {
+                $header[] = "<td>{$heading}</td>";
+            }
+
+            $rowData = array();
+
+            if(count($data) == 0) $rowData[] = "<tr><td colspan='{$columnCount}'>No data</td></tr>";
+
+            if(count($rowData) != 1) {
+
+            	foreach ($data as $row) {
+                	$rowItem = array();
+                	foreach ($row as $key => $val)
+                    	    $rowItem[] = "<td>{$val}</td>";
+
+                	$rowData[] = "<tr>" . implode("\r\n", $rowItem) . "</tr>";
+            	}
+
+        	}
+
+        	return Utility::render($tableTpl, array(
+                	"head" => implode("\r\n", $header),
+                	"data" => implode("\r\n", $rowData)
+            	));
+
+    	}
+
 }
